582fceb2f1adbaabec61f537c706b7e6210208ae
RuthDeveLoper <marceloruth23@gmail.com>
2024-07-04 15:17:03 -0500

    INICIO DE PROYECTO LPII_CL3_MarceloMedranoRuth

---
+++ src/main/java/com/bd/cl3/LpiiCl3MarceloMedranoRuthApplication.java
@@ -0,0 +1,13 @@
+package com.bd.cl3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiCl3MarceloMedranoRuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiCl3MarceloMedranoRuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/cl3/LpiiCl3MarceloMedranoRuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.cl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiCl3MarceloMedranoRuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
